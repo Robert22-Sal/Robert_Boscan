2a39062b7c5e90a84b3ab3fb1f69e2ba7ee75e6a
Robert22-Sal <robert.bbeto.k@gmail.com>
2025-07-30 15:56:05 -0400

    curso de programacion

--- modulo_1/python/Tarea Cesta de Compras.py
+++ modulo_1/python/Tarea Cesta de Compras.py
@@ -3,7 +3,7 @@
 lista = [] 
 precios = []
 print("")
-agregar_a_la_lista = input("Qué deseas agregar a la cesta?").lower()
+agregar_a_la_lista = input("Qué deseas agregar a la cesta?").lower().strip()
 precio1 = float(input("Cuál es el precio de eso?"))
 precios.append(precio1)
 lista.append(agregar_a_la_lista)
@@ -23,26 +23,26 @@ def opciones():
 def comprando():
  while True:
     opciones()
-    pregunta = input("")
+    pregunta = input("").strip().lower()
     
-    if pregunta in ["AGREGAR", "Agregar", "agregar", "1"]:
+    if pregunta in ["agregar", "1"]:
           print()  
-          agregar_a_la_lista = input("Qué deseas agregar a la cesta?").lower()
+          agregar_a_la_lista = input("Qué deseas agregar a la cesta?").lower().strip()
           precio = float(input("Cuál es el precio de eso?"))
           precios.append(precio)
           lista.append(agregar_a_la_lista)
           print(f"haz añadido {agregar_a_la_lista} a la cesta")
           print(f"Con un costo de {precio:.2f}$")
           
-    elif pregunta in ["MOSTRAR", "Mostrar", "mostrar", "2"]:
+    elif pregunta in ["mostrar", "2"]:
         print()
         print(f"tu cesta tiene {lista}")
         
-    elif pregunta in ["Sacar", "SACAR", "sacar", "3"]:
+    elif pregunta in ["sacar", "3"]:
             print()
             print ("Qué deseas sacar de la cesta?")
             print (lista)
-            eliminar = input("").lower()
+            eliminar = input("").lower().strip()
             if eliminar not in lista:
                     print("Ese producto no esta en la cesta.")
                     continue
@@ -50,7 +50,7 @@ def comprando():
             print (f"Haz sacado {eliminar} de la cesta.")
             
             
-    elif pregunta in ["CALCULAR", "Calcular", "calcular", "4"]:
+    elif pregunta in ["calcular", "4"]:
             
             print()
             print("el precio de tus productos es de:")
@@ -58,7 +58,7 @@ def comprando():
             print(F"{precio_total:.2f}")
             
             
-    elif pregunta in ["SALIR", "Salir", "salir", "5"]:
+    elif pregunta in ["salir", "5"]:
           print()
           print("Gracias por su compra!")   
           break
