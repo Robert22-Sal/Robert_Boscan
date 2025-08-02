31ddfd069db04e0249230974be1cc2ad20f83f2c
Robert22-Sal <robert.bbeto.k@gmail.com>
2025-08-02 15:02:46 -0400

    clase 3 modulo 2

--- modulo_2/JavaScript/switch.js
+++ modulo_2/JavaScript/switch.js
@@ -1,37 +1,38 @@
 //ejercicio 1
 
-const diasSemana = "dia numero 7";
+const diasSemana = 7;
 
 switch (diasSemana) {
-    case "1":
+    case 1:
         console.log ("Lunes");
         break;
 
-    case "2":
+    case 2:
         console.log ("martes");
         break;
 
-    case "3":
+    case 3:
         console.log ("miercoles");
         break;
 
-    case "4":
+    case 4:
         console.log ("jueves");
         break;
 
-    case "5":
+    case 5:
         console.log ("viernes");
         break;
 
-    case "6":
+    case 6:
         console.log ("sabado")
         break;
 
-    case "7":
+    case 7:
         console.log ("domingo");
         break;
-
-
+    default:
+        console.log ("ese numero no es valido");
+        break;
 }
 
 // ejercicio 2
