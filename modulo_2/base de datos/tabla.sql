35706f5bce3c86129207bbb88fd881fc3c3bf7f8
Robert22-Sal <robert.bbeto.k@gmail.com>
2025-08-08 14:50:53 -0400

    clase 4 modulo 2

---
+++ modulo_2/base de datos/tabla.sql
@@ -0,0 +1,11 @@
+CREATE TABLE YO (
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    C_I INT,
+);
+
+INSERT INTO YO(nombre, apellido, C_I)
+
+VALUES  ("robert", "boscan", "33.607.751"),
+        ("ana", "hernandez", "6254638524658"),
+		("ari", "gonzales", "64725976236");
